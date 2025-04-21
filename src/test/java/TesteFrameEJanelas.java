6204775689e371ac0996b7801cd3b827695e826f
Vinicius Lourenço <vinicius_lourenco@outlook.com>
2025-04-21 13:23:07 -0300

    feat: add scroll function interageFrameEscondido in TesteFrameEJanelas class

--- src/test/java/TesteFrameEJanelas.java
+++ src/test/java/TesteFrameEJanelas.java
@@ -2,20 +2,21 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteFrameEJanelas {
 
     private WebDriver driver;
+    private final String URL = "file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html"; //URL
+    private DSL dsl;
 
     @Before
     public void inicializa() {
+        driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(1200, 765));
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        dsl = new DSL(driver);
+        driver.get(URL);
     }
 
     @After
@@ -34,6 +35,17 @@ public class TesteFrameEJanelas {
 
         driver.switchTo().defaultContent();
         driver.findElement(By.id("elementosForm:nome")).sendKeys(msg);
+    }
+
+    @Test
+    public void interageFrameEscondido() {
+        WebElement frame = driver.findElement(By.id("frame2"));
+        dsl.executarJS("window.scrollBy(0, arguments[0])", frame.getLocation().y);
+        driver.switchTo().frame("frame2");
+        driver.findElement(By.id("frameButton")).click();
+        Alert alert = driver.switchTo().alert();
+        String msg = alert.getText();
+        Assert.assertEquals("Frame OK!", msg);
     }
 
     @Test
