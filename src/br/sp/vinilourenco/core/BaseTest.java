ba20e1f7562d0ff5a2cf182481cab1ad63b9a263
Vinicius Lourenço <vinicius_lourenco@outlook.com>
2025-04-28 10:57:06 -0300

    feat: implement screenshots after all tests

--- src/br/sp/vinilourenco/core/BaseTest.java
+++ src/br/sp/vinilourenco/core/BaseTest.java
@@ -1,13 +1,30 @@
 package br.sp.vinilourenco.core;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
+import java.io.File;
+import java.io.IOException;
+
+import static br.sp.vinilourenco.core.DriverFactory.getDriver;
 import static br.sp.vinilourenco.core.DriverFactory.killDriver;
 
 public class BaseTest {
 
+    @Rule
+    public TestName testName = new TestName();
+
     @After
-    public void finaliza() {
+    public void finaliza() throws IOException {
+        TakesScreenshot ss = (TakesScreenshot) getDriver();
+        File archive = ss.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(archive, new File("target" + File.separator + "screenshot" +
+                File.separator + testName.getMethodName() + ".jpg"));
+
         if (Properties.FECHAR_BROWSER) {
             killDriver();
         }
