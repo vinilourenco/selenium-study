dd0c98d1243427886ea5d55bc1d504ee4668b61c
Vinicius Lourenço <vinicius_lourenco@outlook.com>
2025-04-28 11:04:34 -0300

    feat: implement browser switching functionality

--- src/br/sp/vinilourenco/core/DriverFactory.java
+++ src/br/sp/vinilourenco/core/DriverFactory.java
@@ -3,6 +3,7 @@ package br.sp.vinilourenco.core;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 
@@ -12,7 +13,10 @@ public class DriverFactory {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            driver = new ChromeDriver();
+            switch (Properties.browser) {
+                case FIREFOX -> driver = new FirefoxDriver();
+                case CHROME -> driver = new ChromeDriver();
+            }
             driver.manage().window().setSize(new Dimension(1200, 765));
         }
         return driver;
--- src/br/sp/vinilourenco/core/Properties.java
+++ src/br/sp/vinilourenco/core/Properties.java
@@ -4,4 +4,11 @@ public class Properties {
 
     public static boolean FECHAR_BROWSER = false;
 
+    public static Browser browser = Browser.CHROME;
+
+    public enum Browser {
+        CHROME,
+        FIREFOX
+    }
+
 }
